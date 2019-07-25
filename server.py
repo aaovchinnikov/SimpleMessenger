94fe5bf153369be7b75e018c1ee46daf548dee06
LebedevaAlexandraVictorovna <avlebedeva_4@miem.hse.ru>
2019-07-25 16:50:33 +0300

    Убрано приветственное сообщение из сервера

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
 
     name = client.recv(1024).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    welcome = '%s' % name
     client.send(bytes(welcome, "utf8"))
     clients[client] = name
 
