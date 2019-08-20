0636d1c928eb3073e4ec51ef01159fd66830737a
LebedevaAlexandraVictorovna <avlebedeva_4@miem.hse.ru>
2019-08-20 21:59:20 +0300

    Обновлен клиент

--- client2.py
+++ client2.py
@@ -7,27 +7,16 @@ except ImportError:
 
 def send_message(event=None):  # event is passed by binders.
 
-    msg = txt.get()
-    txt.set("")  # Clears input field.
+    msg = txt1.get()
+    msg1 = name + ": " + msg
+    chat_field.insert(tk.END, msg1)
+    msg = friend + "$" + msg
+    txt1.set("")  # Clears input field.
     client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
-        client_socket.close()
-        main.quit()
-
-
-def receive_message():
-    while True:
-        try:
-            msg = client_socket.recv(1024).decode("utf8")
-            chat_list.insert(tk.END, msg+'\n')
-        except OSError:  # Possibly client has left the chat.
-            break    
 
 
 def on_closing(event=None):
     """This function is to be called when the window is closed."""
-    #txt.set("{quit}")
-    #send_message()
     msg = "{quit}"
     client_socket.send(bytes(msg, "utf8"))
     client_socket.close()
@@ -35,18 +24,42 @@ def on_closing(event=None):
     
 
 def update_chat_list():
+    global dialog
+    global friend
+    global name
     while True:
         try:
             
             msg = client_socket.recv(1024).decode("utf8")
-            chat_list.delete(0, tk.END)
-            msg_buf=""
-            for i in msg:
-                if i != "*":
-                    msg_buf = msg_buf + i
+            if msg[0]=="$": #message for chatting
+                friend1 = friend
+                magia=msg[1:].split("$")
+                msg=magia[2]
+                if magia[0]==name:
+                    friend = magia[1]
                 else:
-                    chat_list.insert(tk.END, msg_buf)
-                    msg_buf=""
+                    friend = magia[0]           
+                msg = friend + ": " + msg    
+                
+
+                if dialog == 0: 
+                    chat.deiconify()
+                    label4['text'] = friend
+                    dialog = 1
+                if friend1 != friend:
+                    label4['text'] = friend
+                    chat_field.delete(0, tk.END)
+
+                chat_field.insert(tk.END, msg)
+            else: #updating chat list
+                chat_list.delete(0, tk.END)
+                msg_buf=""
+                for i in msg:
+                    if i != "*":
+                        msg_buf = msg_buf + i
+                    else:
+                        chat_list.insert(tk.END, msg_buf)
+                        msg_buf=""
     
         except OSError:  # Possibly client has left the chat.
             break
@@ -55,6 +68,7 @@ def update_chat_list():
 
 def registration(event=None):  # event is passed by binders.
     """Function to introduce yourself"""
+    global name
     name = txt.get()
     client_socket.send(bytes(name, "utf8"))
     label1.destroy()
@@ -62,22 +76,21 @@ def registration(event=None):  # event is passed by binders.
     entry_field.destroy()
 
 def chatting(event=None):
-    print("LOL")
-    chat = tk.Toplevel(main)
-    chat_frame1 = tk.Frame(chat)
-    chat_frame1.pack(side = tk.BOTTOM)
-    chat.geometry("340x150")
-    scrollbar1 = tk.Scrollbar(chat_frame1)
-    label4 = tk.Label(chat, text = "qwert")
-    label4.pack(side = tk.TOP)
-    chat_field = tk.Listbox(chat_frame1, height=10, width=50, yscrollcommand=scrollbar1.set)
-    chat_field.pack(side=tk.LEFT, fill=tk.BOTH)
-    chat_field.pack()
-    scrollbar1.pack(side=tk.RIGHT, fill=tk.Y) 
-
-
-
-
+    global friend
+    friend1 = friend
+    w = event.widget
+    i = int(w.curselection()[0])
+    friend = w.get(i)
+    #print(friend)
+    global dialog
+    
+    dialog = 1
+    chat.deiconify()
+    label4['text'] = friend
+    if friend1 != friend:
+        chat_field.delete(0, tk.END)
+    
+name=""
 main = tk.Tk() 
 main.title("Simple Messenger")
 
@@ -102,9 +115,33 @@ scrollbar = tk.Scrollbar(chat_frame)
 chat_list = tk.Listbox(chat_frame, height=10, width=50, yscrollcommand=scrollbar.set)
 chat_list.pack(side=tk.LEFT, fill=tk.BOTH)
 chat_list.pack()
+
+txt1 = tk.StringVar()
+dialog = 0 # flag 
+friend = ""
 chat_list.bind('<Double-Button-1>', chatting)
 chat_frame.pack()
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y) 
+
+#dialog
+chat = tk.Toplevel(main)
+label4 = tk.Label(chat, text = friend, bg = "lightgreen", font = 'arial 14')
+label4.pack(side = tk.TOP)
+chat_frame1 = tk.Frame(chat)
+chat_frame1.pack()
+chat.geometry("340x220")
+scrollbar1 = tk.Scrollbar(chat_frame1)
+
+chat_field = tk.Listbox(chat_frame1, height=10, width=50, yscrollcommand=scrollbar1.set)
+chat_field.pack(side=tk.LEFT, fill=tk.BOTH)
+chat_field.pack()
+scrollbar1.pack(side=tk.RIGHT, fill=tk.Y) 
+entry_field1 = tk.Entry(chat, textvariable=txt1, width=56)
+
+entry_field1.bind("<Return>", send_message)
+entry_field1.pack(side = tk.BOTTOM)
+
+chat.withdraw()
 
 
 main.protocol("WM_DELETE_WINDOW", on_closing) 
